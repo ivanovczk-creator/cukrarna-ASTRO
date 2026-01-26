79c3081be61e5047cdc7bedc18ea2bd5665f7656
ivanovczk-creator <ivanovczk@gmail.com>
2026-01-27 00:15:41 +0100

    Replace sitemap integration with tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://cukrarstviblahutovi.cz',
-  integrations: [
-    sitemap(),
-    react()
-  ],
+  integrations: [tailwind(), react()],
 });
