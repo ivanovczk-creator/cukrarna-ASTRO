1779038977395465fe1547dd9651bf0fe3695790
ivanovczk-creator <ivanovczk@gmail.com>
2026-01-27 00:16:36 +0100

    Add Tailwind CSS configuration file

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          900: '#0a192f',
+          800: '#112240',
+        },
+        gold: {
+          500: '#d4af37',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
