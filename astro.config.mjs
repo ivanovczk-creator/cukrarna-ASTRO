da0342d2f026dc6e5dcdaa876dc5c1c219975c0b
ivanovczk-creator <ivanovczk@gmail.com>
2026-01-29 17:14:50 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap'; // 1. Musíme přidat tento import
 
-// Nyní bezpečně integrujeme Tailwind přímo do sestavení webu
 export default defineConfig({
+  // 2. TENTO ŘÁDEK JE KLÍČOVÝ - bez něj sitemapa nevznikne
+  site: 'https://www.cukrarstviblahutovi.cz', 
   integrations: [
     react(), 
-    tailwind()
+    tailwind(),
+    sitemap() // 3. Přidáme sitemapu do seznamu integrací
   ],
 });
